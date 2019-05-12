3f2d9fc0adb5c6c95bb89ad7c41bacf91e81b198
Vadim <boom000@ya.ru>
2019-05-13 00:08:35 +0300

    Add server commit

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,35 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await new Promise((resolve) => {
+    subscribers.push(resolve);
+
+    ctx.req.on('close', () => {
+      subscribers.splice(subscribers.indexOf(resolve), 1);
+    });
+  });
+
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  if (!message) {
+    ctx.body = 'NO MESSAGE';
+    return;
+  }
+
+  subscribers.forEach((resolve) => {
+    resolve(message);
+  });
+
+  subscribers = [];
+  ctx.body = 'OK';
+
+  return next();
 });
 
 app.use(router.routes());
