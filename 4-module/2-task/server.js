8a6c6e6f654870a3f7ec8f1f24668e35045ad5ed
Vadim <boom000@ya.ru>
2019-05-12 23:58:36 +0300

    Add server commit

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,59 +1,37 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const LimitSizeStream = require('./LimitSizeStream');
-let writeFile,limitFile;
-const server = new http.Server();
+const Koa = require('koa');
+const app = new Koa();
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+app.use(require('koa-static')('public'));
+app.use(require('koa-bodyparser')());
 
-  const filepath = path.join(__dirname, 'files', pathname);
+const Router = require('koa-router');
+const router = new Router();
+const subscribers = [];
 
-  switch (req.method) {
-    case 'POST':
-    if (pathname.includes('/')) {
-      res.statusCode = 400;
-      res.end('Not supported');
-};
-    writeFile = fs.createWriteStream(filepath, {"flags": "wx"});
-    limitFile = new LimitSizeStream({limit: 1024*1024});
-
-    limitFile.on('error', () => {
-      fs.unlink(filepath, () => {});
-      res.statusCode = 413;
-      res.end('To big size');
-});
-    
-    writeFile.on('error', (error) => {
-      if (error.code === 'EEXIST') {
-        res.statusCode = 409;
-        res.end('File is  exists');
-      }
+router.get('/subscribe', async (ctx, next) => {
+    const message = await new Promise((resolve) => {
+      subscribers.push(resolve.bind(this));
     });
 
-    req
-     .pipe(limitFile)
-     .pipe(writeFile)
-
-       writeFile.on('close', () => {
-       res.statusCode = 201;
-       res.end('Success upload');
-    });
-
-    req.on('close', () => {
-      fs.unlink(filepath, () => {
-        res.statusCode = 500;
-        res.end('Somthing went wrong');
-      });
-    });
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+    ctx.status = 200;
+    ctx.body = message;
   }
+);
+
+router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (message) {
+    let size = subscribers.length;
+    while (size--) {
+      subscribers.pop()(message);
+    }
+  }
+
+  ctx.status = message ? 200 : 204;
+  ctx.body = message ? 'Success' : 'No message';
 });
 
-module.exports = server;
+app.use(router.routes());
+
+module.exports = app;
